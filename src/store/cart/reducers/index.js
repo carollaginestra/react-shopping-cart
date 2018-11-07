db3f4b8386e70e8694951b77a8a2bb9dccf3b858
Carolina <carolinalaginestra@hotmail.com>
2018-11-07 17:48:37 -0200

    add localStorage

--- src/store/cart/reducers/index.js
+++ src/store/cart/reducers/index.js
@@ -1,5 +1,5 @@
 const initialState = {
-    products: [],
+    products: JSON.parse(localStorage.getItem('PRODUCTS')) || [],
     sort: localStorage.getItem('SORT') || 'asc',
     items: [],
 };
@@ -9,18 +9,21 @@ export default (state = initialState, action) => {
 
     switch (type) {
         case 'CART_ADD_PRODUCT': {
+            localStorage.setItem('PRODUCTS', JSON.stringify([...state.products, payload.product]));
             return {
                 ...state,
                 products: [...state.products, payload.product],
             };
         }
         case 'CART_REMOVE_PRODUCT': {
+            localStorage.setItem('PRODUCTS', JSON.stringify(state.products.filter(({ id }) => id !== payload.id)));
             return {
                 ...state,
                 products: state.products.filter(({ id }) => id !== payload.id),
             };
         }
         case 'CART_REMOVE_ALL_PRODUCTS': {
+            localStorage.setItem('PRODUCTS', JSON.stringify([]));
             return {
                 ...state,
                 products: [],
